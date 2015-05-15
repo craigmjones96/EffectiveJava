1146c8f017a09d4aa02a372ea44cf03dc8db2bfa
craigmjones96 <craigmjones96@gmail.com>
2015-05-15 11:55:07 +0100

    StaticFactoryMethodConstructor

---
+++ StaticFactoryMethod/src/com/craig/constructor/ConstructorMain.java
@@ -0,0 +1,14 @@
+package com.craig.constructor;
+
+public class ConstructorMain {
+
+	public static void main(String[] args) {
+		// Dog molly = new Dog("Molly");
+		// Dog sasha = new Dog();
+		// this wont work as we made a private consructor, which means there is no constructor 
+		// (with or without parameters) available to us now.
+		
+		Dog bowser = Dog.newDog("Bowser");
+		System.out.println(bowser.toString());
+	}
+}
---
+++ StaticFactoryMethod/src/com/craig/constructor/Dog.java
@@ -0,0 +1,18 @@
+package com.craig.constructor;
+
+public class Dog {
+	
+	private String name;
+	
+	private Dog(String name){
+		this.name = name;
+	};
+	
+	public static Dog newDog(String name) {
+		return new Dog(name);
+	}
+	
+	public String toString() {
+		return "This dog is called " + name;
+	}
+}
